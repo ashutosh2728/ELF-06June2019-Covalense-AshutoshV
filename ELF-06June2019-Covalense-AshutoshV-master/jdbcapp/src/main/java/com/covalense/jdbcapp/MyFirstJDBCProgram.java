7159af1ee5b48837daf634071ca1b389a703dd83
Ashutosh Verma <ashutosh.verma26@gmail.com>
2019-07-01 17:56:24 +0530

    This is java file.

---
+++ ELF-06June2019-Covalense-AshutoshV-master/jdbcapp/src/main/java/com/covalense/jdbcapp/MyFirstJDBCProgram.java
@@ -0,0 +1,71 @@
+package com.covalense.jdbcapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Driver;
+
+import lombok.extern.java.Log;
+
+@Log
+public class MyFirstJDBCProgram {
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			// 1. Load the Driver
+			Driver driver = new Driver();
+			DriverManager.deregisterDriver(driver);
+
+			// 2. Get the DB Connection via Driver
+			String dbUrl = "jdbc:mysql://localhost:3306/covalense_db?user=root&password=root";
+			con = DriverManager.getConnection(dbUrl);
+
+			// 3. Issue "SQL Queries via Connection"
+			String query = "select * from employee_info";
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+
+			// 4. Process the results returned by Sql queries
+			while (rs.next()) {
+				log.info("ID ===>" + rs.getInt("ID"));
+				log.info("NAME ===>" + rs.getString("NAME"));
+				log.info("AGE ===>" + rs.getInt("AGE"));
+				log.info("GENDER ===>" + rs.getString("GENDER"));
+				log.info("SALARY ===>" + rs.getDouble("SALARY"));
+				log.info("PHONE ===>" + rs.getInt("PHONE"));
+				log.info("JOINING_DATE ===>" + rs.getDate("JOINING_DATE"));
+				log.info("ACCOUNT_NUMBER ===>" + rs.getInt("ACCOUNT_NUMBER"));
+				log.info("EMAIL ===>" + rs.getString("EMAIL"));
+				log.info("DESIGNATION ===>" + rs.getString("DESIGNATION"));
+				log.info("DOB ===>" + rs.getDate("DOB"));
+				log.info("DEPT_ID ===>" + rs.getInt("DEPT_ID"));
+				log.info("MNGR_ID ===>" + rs.getInt("MNGR_ID"));
+			} // End of while
+
+		} catch (SQLException e) {
+			e.getMessage();
+		} finally {
+			// 5. Close All "JDBC Objects"
+			{
+				try {
+					if (con != null) {
+						con.close();
+					}
+					if (stmt != null) {
+						stmt.close();
+					}
+					if (rs != null) {
+						rs.close();
+					}
+				} catch (SQLException e) {
+					e.getMessage();
+				}
+			}
+		}
+	}
+}
