7ab64b9b93049db040a5ba84611538b0a11a84ea
Ashutosh Verma <ashutosh.verma26@gmail.com>
2019-07-22 14:00:11 +0530

    this is updated file.

--- ELF-06June2019-Covalense-AshutoshV-master/Assessment/Frontend/RealTimeEditor.js
+++ ELF-06June2019-Covalense-AshutoshV-master/Assessment/Frontend/RealTimeEditor.js
@@ -1,4 +1,4 @@
-   
+//  a function first it take the element and shows the output in a output window
 function process() {
     document.getElementById("output").innerHTML=document.getElementById("in").inneHTML=document.getElementById("in").value;
 }
